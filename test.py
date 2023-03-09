7d882a1cd6f5725c4ff2193a2d07c28fbaacd6e3
alex174 <axchang777@gmail.com>
2023-03-09 12:07:47 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>Hello, World!  This is an internal app page!</h1>'
+
+@app.route('/facedetector',methods=['GET'])
+def facedetector():
+    args=request.args
+    face_left=args.get("face_left")
+    face_top=args.get("face_top")
+    face_right=args.get("face_right")
+    face_bottom=args.get("face_bottom")
+    face_id=args.get("face_id")
+    info="New face with ID: " + face_id + " detected at X pos " + face_left + " and Y pos " + face_right
+    return info
